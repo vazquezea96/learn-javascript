fa042cab64089ae42001c3022b073636da4ef30e
vazquezea96 <Eduardo.Vazquez@seattlecolleges.edu>
2022-12-01 00:19:37 -0800

    Day 1: solved

--- Javascriptmas/panic_function.js
+++ Javascriptmas/panic_function.js
@@ -4,20 +4,13 @@ second: split sentence if phrase or sentence is longer than single word
 third: add emoji in between each word 
 fourth: join string together to output: uppercase, emoji inbetween plus !
 */
+
 function panic(sentence) {
-    const sentenceSplit = sentence.split();
-    if (sentenceSplit.length === 1) {
-        return sentence.toUpperCase() + "!";
-    } else if (sentenceSplit.length !== 1) {
-        return sentence.toUpperCase().split(' ').join(' 😱 ') + '!';
-    }
+    let newSentence = sentence.toUpperCase();
+    newSentence = sentence.replaceAll(' ', ' 😱 ')
+    return `${newSentence.toUpperCase()}!`
+  
 }
 
 console.log(panic("I'm almost out of coffee")); 
 console.log(panic("winter is coming"))
-
-/* 
-arrow function:
-
-const panic = (sentence) => 
-*/
